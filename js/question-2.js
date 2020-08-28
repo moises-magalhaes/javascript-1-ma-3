39d08712aebfc4734762e8c1148c808b9e84f1aa
moises-magalhaes <63241484+moises-magalhaes@users.noreply.github.com>
2020-08-28 15:03:53 +0200

    results, rating and tag fixed

--- js/question-2.js
+++ js/question-2.js
@@ -1,6 +1,12 @@
 const url = "https://api.rawg.io/api/games?dates=2019-01-01,2019-12-31&ordering=-rating";
 
 const resultContainer = document.querySelector(".results");
+const loading = document.querySelector(".loading");
+
+setTimeout(function () {
+    loading.classList.remove(".loading-symbol");
+}, 5000);
+
 
 async function makeApiCall() {
     try {
@@ -12,20 +18,24 @@ async function makeApiCall() {
 
 
         const jsonCount = result.results;
+        const jsonRating = result.results;
+        const jsonTags = result.results;
 
         resultContainer.innerHTML = "";
 
         for (let i = 0; i <= jsonCount.length; i++) {
             console.log(jsonCount[i]);
+            console.log(jsonRating[i].rating);
+            console.log(jsonTags[i].tags.length);
+
             if (i === 8) {
                 break;
-            }
+            };
+            resultContainer.innerHTML += `<div class="json-1"><p>name: ${jsonCount[i].name} </p></div>`;
 
-            resultContainer.innerHTML += `<div class="json">${jsonCount[i].name}</div>`;
+            resultContainer.innerHTML += `<div class="json-2"><p>rating: ${jsonRating[i].rating}</p></div>`;
 
-            resultContainer.innerHTML += `<div class="json">${jsonCount[i].tags}</div>`;
-
-            resultContainer.innerHTML += `<div class="json">${jsonCount[i].ratings}</div>`;
+            resultContainer.innerHTML += `<div class="json-3"><p>tags: ${jsonTags[i].tags.length}</p></div>`;
         }
     }
     catch (error) {
@@ -34,4 +44,4 @@ async function makeApiCall() {
     }
 };
 
-makeApiCall(url);
+makeApiCall();
